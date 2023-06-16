2bd5d536738ec49f6ae34081d59ca79b048ea725
RJ <rjay.tedoco@gmail.com>
2023-06-16 12:06:11 +0800

    Add reservation functionality

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: String;
@@ -11,4 +11,14 @@ export type SafeUser = Omit<
   createdAt: String;
   updatedAt: String;
   emailVerified: string | null;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
 };
